0dd8b26d7ec30fc14d8a6b66a36e7beb814d8ea0
LB_mbp <31675618+boylach@users.noreply.github.com>
2018-05-15 14:59:22 +1000

    gpio setup alterations

--- routes/ajax.js
+++ routes/ajax.js
@@ -6,6 +6,7 @@ var rpio = require('rpio');
 module.exports = function(req, res) {
     
     if (req.body.hasOwnProperty('action')) {
+        rpio.init({mapping: 'gpio'});
         switch( req.body.action) {
             case 'write': 
                 /*gpio.setup(req.body.gpio, gpio.DIR_OUT, function() {
@@ -61,20 +62,26 @@ module.exports = function(req, res) {
                 break;
 
             case 'carriage C':
-                rpio.init({mapping: 'gpio'});
-                rpio.open(req.body.gpio, rpio.OUTPUT, + req.body.status);
-                rpio.write(req.body.gpio, + req.body.status);
-                res.contentType('json');
-                res.send({ gpio: req.body.gpio, status: req.body.status });
+            //    rpio.init({mapping: 'gpio'});
+            //    rpio.open(req.body.gpio, rpio.OUTPUT, + req.body.status);
+            //    rpio.write(req.body.gpio, + req.body.status);
+            //    res.contentType('json');
+            //    res.send({ gpio: req.body.gpio, status: req.body.status });
 
                 console.log("focus C");
                 console.log((req.body.status) ? "forward" : "back");
 
                 // set direction
                 dirPin = 17;
+                
+                rpio.open(req.body.gpio, rpio.OUTPUT, + req.body.status);
                 rpio.write(dirPin, req.body.status);
+            //    res.contentType('json');
+
                 console.log("76");
                 console.log(req.body.gpio);
+            //    res.send({ gpio: req.body.gpio, status: req.body.status });
+
                 // test: do a full revolution of the carriage motion thing
                 gpioPin = 26; // for GPIO 26
                 for(var i = 0; i < (2*200*16) - 1; i++){ // (stepsPerRev * microstepping * 2) - 1. (as it's a half cycle) - is there a fencepost error here?
@@ -82,10 +89,11 @@ module.exports = function(req, res) {
                     sleaep(1); 
                     rpio.write(gpioPin, !rpio.read(gpioPin));
                 }
+                res.contentType('json');
+                res.send({ gpio: gpioPin, status: rpio.read(gpioPin) }); 
 
                 break;
         }
-      
     }                                           
     // res.contentType('json');                 
     // res.send({ some: req.body.action });     
